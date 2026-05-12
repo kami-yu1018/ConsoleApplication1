dc2ba0c54e51192b7cfc16e9cc88a64d4b9c18ae
kami-yu <yuzukano0181@outlook.jp>
2026-05-12 16:02:52 +0900

    ・for文をwhile文に変更 ・if分の条件を論理積での判定に変更 ・変数iを追加

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -13,10 +13,17 @@ int main()
     //  テキストを反転
     std::reverse(text_rev.begin(), text_rev.end());
 
-    for (int i = 0; i < 100; ++i) 
+    //  型推論（今回はint型）
+    auto i = 10;
+
+//    for (int i = 0; i < 100; ++i) 
+    while(i--)
     {
         //  ２回に１回反転した文字列を出力する
-        if (i % 2 == 0)
+//        if (i % 2 == 0)
+        //  論理積で判定
+        //  余剰よりも論理積での判定の法が処理が軽い
+        if(i & 1)
         {
             //  出力
             std::cout << text << std::endl;
